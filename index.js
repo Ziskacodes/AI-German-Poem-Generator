65b5de5bb7835b2206b7333247f4050ac4e3ff6d
Ziskacodes <fr.hennings85@gmail.com>
2025-06-27 09:51:26 +0200

    js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function generatePoem (event) {
     event.preventDefault();
-    let poemElement= document.querySelector(".poem");
+    document.querySelector(".poem");
    new Typewriter('#poem', {
         strings: ['Generating Poem'],
         autoStart: true,
         delay: 150,
-      });;
+      });
     
 }
 document.querySelector("form").addEventListener("submit", generatePoem)
